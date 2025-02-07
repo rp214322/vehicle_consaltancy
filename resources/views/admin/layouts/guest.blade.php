a97303ca71448d8d8beb340462212d2fb85518eb
RiteshP-Shivlab <ritesh.patel@shivlab.com>
2025-02-07 12:37:39 +0530

    favicon logo set

--- resources/views/admin/layouts/guest.blade.php
+++ resources/views/admin/layouts/guest.blade.php
@@ -5,7 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Laravel') }} Admin</title>
+        <link rel="shortcut icon" href="{{ asset('images/facivon.png') }}">
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
